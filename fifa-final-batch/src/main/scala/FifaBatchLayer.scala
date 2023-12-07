a4059161f63cdc4027746f54547a81567048fd0d
LoombaSetu <setu@uchicago.edu>
2023-12-07 01:41:36 -0600

    functioning batch layer

---
+++ fifa-final-batch/src/main/scala/FifaBatchLayer.scala
@@ -0,0 +1,114 @@
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql.functions.{col, _}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.ml.regression.DecisionTreeRegressor
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+
+object FifaBatchLayer {
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+      .setAppName("Build FIFA Players Table")
+      .setMaster("local[2]")
+
+    val spark = SparkSession.builder()
+      .appName("FIFA Players Table")
+      .config("spark.hadoop.metastore.catalog.default", "hive")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    try {
+      // Read data from Hive Table
+      val playerData = spark.sql("SELECT *, current_timestamp() as time_stamp FROM playerdatahive")
+      val latest_timestamp = playerData
+        .orderBy(desc("time_stamp"))
+        .limit(1)
+        .select("time_stamp")
+        .first()
+        .getAs[java.sql.Timestamp](0)
+      val inputPlayerData = spark.sql(s"SELECT * FROM newplayerdata WHERE time_stamp > '$latest_timestamp'")
+
+      // Feature selection
+      val selectedFeatures = Array(
+        "pace", "shooting", "passing", "dribbling", "defending", "physic", "club_name",
+        "height_cm", "nationality_name", "preferred_foot", "player_positions"
+      )
+
+      // Select relevant features and target variables
+      val data = playerData.select((selectedFeatures.map(col) :+ col("value_eur")): _*)
+
+      // StringIndexer for categorical variables
+      val categoricalCols = Array("nationality_name", "preferred_foot", "player_positions", "club_name")
+      val indexers = categoricalCols.map(col => new StringIndexer().setInputCol(col).setOutputCol(col + "_index"))
+
+      // Assemble features into a vector
+      val assembler = new VectorAssembler()
+        .setInputCols((selectedFeatures ++ categoricalCols.map(_ + "_index")).distinct)
+        .setOutputCol("features")
+
+      // Decision Tree Regression Model
+      val dt = new DecisionTreeRegressor()
+        .setLabelCol("overall") // Change this to "value_eur" for different target variables
+        .setFeaturesCol("features")
+
+      // Create a pipeline with stages
+      val pipeline = new Pipeline().setStages(indexers ++ Array(assembler, dt))
+
+      // Split the data into training and testing sets
+      val dataArray = data.randomSplit(Array(0.8, 0.2))
+      val Array(trainingData, testData) = dataArray
+
+      // Train the model
+      val model = pipeline.fit(trainingData)
+
+      // Make predictions on the test set
+      val predictions = model.transform(testData)
+
+      // Evaluate the model
+      val evaluator = new RegressionEvaluator()
+        .setLabelCol("value_eur")
+        .setPredictionCol("prediction")
+        .setMetricName("rmse")
+
+      val rmse = evaluator.evaluate(predictions)
+      println(s"Root Mean Squared Error (RMSE) on test set: $rmse")
+
+      // Save the model
+      model.write.overwrite().save("hdfs:///setu-project/")
+
+      // predictions for input data
+      val inputPredictions = model.transform(inputPlayerData)
+      val inputPredictionsSubset = inputPredictions.select("short_name", "fifa_version", "prediction")
+      val predictionsFinal = inputPredictionsSubset.withColumnRenamed("prediction", "value_eur")
+      val inputPlayerDataProcessed = inputPlayerData
+        .join(predictionsFinal, Seq("short_name", "fifa_version"), "left_outer")
+
+      // Put inputPredictions to "value_eur" column of inputPlayerData and then combine the datasets
+      val combinedData = playerData.union(inputPlayerDataProcessed)
+
+      // Creating views and saving to Hbase via Hive
+      spark.sql("insert overwrite table playerdataprocessed " +
+        "select concat(short_name, fifa_version)," +
+        "  fifa_version, short_name, long_name, height_cm," +
+        "  player_positions, nationality_name, age, club_name," +
+        "  preferred_foot, overall, pace, shooting, passing, dribbling, defending, physic," +
+        "  value_eur from playerdatahive;")
+
+      combinedData.createOrReplaceTempView("combined_data_final")
+
+      // Overwrite the playerdatahive table with the data from combinedData
+      spark.sql("INSERT OVERWRITE TABLE playerdatahive SELECT * FROM combined_data_final")
+      spark.sql("DELETE FROM newplayerdata WHERE time_stamp < '$latest_timestamp'")
+
+
+    } catch {
+      case e: Exception =>
+        // Log or handle the exception
+        e.printStackTrace()
+    } finally {
+      spark.stop()
+    }
+  }
+}
